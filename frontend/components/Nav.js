c609493271cecf7420ef6dd912e0fd03cb4ea89c
AR123456 <coloradoz99@gmail.com>
2018-12-10 16:27:53 -0700

    getting shopping cart to local state

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
@@ -23,6 +25,9 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}> My cart </button>}
+            </Mutation>
           </>
         )}
         {!me && (
