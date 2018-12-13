0dae181cbbbd1d6f5f4935704d864cc747ec5d74
AR123456 <coloradoz99@gmail.com>
2018-12-13 04:53:27 -0700

    display items and remove from cart

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -248,6 +248,31 @@ const Mutations = {
       },
       info
     );
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{id, user {id}}`
+    );
+    //make sure item is found
+    if (!cartItem) throw new Error("No CartItem Found ");
+
+    // make sure user owns that  cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error("Cheating huhhhh");
+    }
+    // del the cart item
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
   }
 };
 
