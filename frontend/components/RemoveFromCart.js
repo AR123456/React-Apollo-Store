ea01e0a43bcf7b4469c64c399b5853cb4b5dafda
AR123456 <coloradoz99@gmail.com>
2018-12-14 06:44:07 -0700

    Optimistic Response and Cache updates

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -27,10 +27,34 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+  //function fets called as soon as response back from server after mutation performed to make the del item snappy
+  update = (cache, payload) => {
+    console.log("Running remove from cart update fn");
+
+    // read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    console.log(data);
+    //remove item from cart
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+
+    // write it back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
+
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
+        update={this.update}
+        // this is making it snappy
+        optimisticResponse={{
+          __typename: "Mutation",
+          removeFromCart: {
+            __typename: "CartItem",
+            id: this.props.id
+          }
+        }}
         variables={{ id: this.props.id }}
       >
         {(removeFromCart, { loading, error }) => (
