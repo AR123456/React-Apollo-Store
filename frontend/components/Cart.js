1863d39f66f7f76d2630051cbe4d3a5bd66c2d83
AR123456 <coloradoz99@gmail.com>
2018-12-20 04:57:41 -0700

    card-process-stripe

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -10,7 +10,7 @@ import SickButton from "./styles/SickButton";
 import CartItem from "./CartItem";
 import calcTotalPrice from "../lib/calcTotalPrice";
 import formatMoney from "../lib/formatMoney";
-
+import TakeMyMoney from "./TakeMyMoney";
 const LOCAL_STATE_QUERY = gql`
   query {
     cartOpen @client
@@ -56,7 +56,9 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            <TakeMyMoney>
+              <SickButton>Checkout</SickButton>
+            </TakeMyMoney>
           </footer>
         </CartStyles>
       );
