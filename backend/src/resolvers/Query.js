58212235afa53f31a4700ff9778f85cb7526887f
AR123456 <coloradoz99@gmail.com>
2018-12-23 06:57:58 -0700

    display single order

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -6,7 +6,7 @@ const Query = {
   item: forwardTo("db"),
   itemsConnection: forwardTo("db"),
   me(parent, args, ctx, info) {
-    // check if there is a a current user ID
+    // check if there is a current user ID
     if (!ctx.request.userId) {
       return null;
     }
@@ -18,16 +18,39 @@ const Query = {
     );
   },
   async users(parent, args, ctx, info) {
-    // check if they are logged in
+    // 1. Check if they are logged in
     if (!ctx.request.userId) {
-      throw new Error("You mst be logged in ! ");
+      throw new Error("You must be logged in!");
     }
-
-    /// check if user has the permissions to query all of the users
+    console.log(ctx.request.userId);
+    // 2. Check if the user has the permissions to query all the users
     hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
 
-    // if they do query all users
+    // 2. if they do, query all the users!
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You arent logged in!");
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
+    // 3. Check if the have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+    if (!ownsOrder || !hasPermission) {
+      throw new Error("You cant see this buddd");
+    }
+    // 4. Return the order
+    return order;
   }
 };
 
--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -11,6 +11,7 @@ import CartItem from "./CartItem";
 import calcTotalPrice from "../lib/calcTotalPrice";
 import formatMoney from "../lib/formatMoney";
 import TakeMyMoney from "./TakeMyMoney";
+
 const LOCAL_STATE_QUERY = gql`
   query {
     cartOpen @client
