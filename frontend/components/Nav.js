da3022970809ebb73e2dad5e0c1cfc3cb5c8ece0
AR123456 <coloradoz99@gmail.com>
2018-12-30 05:12:18 -0700

    testing pages

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,7 +1,6 @@
-import React from "react";
+import Link from "next/link";
 import { Mutation } from "react-apollo";
 import { TOGGLE_CART_MUTATION } from "./Cart";
-import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import CartCount from "./CartCount";
@@ -10,9 +9,9 @@ import Signout from "./Signout";
 const Nav = () => (
   <User>
     {({ data: { me } }) => (
-      <NavStyles>
+      <NavStyles data-test="nav">
         <Link href="/items">
-          <a> Shop</a>
+          <a>Shop</a>
         </Link>
         {me && (
           <>
@@ -29,7 +28,7 @@ const Nav = () => (
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               {toggleCart => (
                 <button onClick={toggleCart}>
-                  My cart
+                  My Cart
                   <CartCount
                     count={me.cart.reduce(
                       (tally, cartItem) => tally + cartItem.quantity,
@@ -43,7 +42,7 @@ const Nav = () => (
         )}
         {!me && (
           <Link href="/signup">
-            <a>Signin</a>
+            <a>Sign In</a>
           </Link>
         )}
       </NavStyles>
