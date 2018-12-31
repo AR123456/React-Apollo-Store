e01d30b3debe826f14bc3e86db13c9732eeba9ab
AR123456 <coloradoz99@gmail.com>
2018-12-31 06:55:06 -0700

    testing mutations

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -41,7 +41,6 @@ class CreateItem extends Component {
   };
 
   uploadFile = async e => {
-    console.log("uploading file...");
     const files = e.target.files;
     const data = new FormData();
     data.append("file", files[0]);
@@ -55,7 +54,6 @@ class CreateItem extends Component {
       }
     );
     const file = await res.json();
-    console.log(file);
     this.setState({
       image: file.secure_url,
       largeImage: file.eager[0].secure_url
@@ -66,6 +64,7 @@ class CreateItem extends Component {
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
           <Form
+            data-test="form"
             onSubmit={async e => {
               // Stop the form from submitting
               e.preventDefault();
