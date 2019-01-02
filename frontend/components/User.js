7bb191f25bc872b53405620f6f780185d1ec2f6b
AR123456 <coloradoz99@gmail.com>
2019-01-02 05:04:36 -0700

    testing cart items

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,62 +1,36 @@
-import React, { Component } from "react";
-import { Mutation } from "react-apollo";
+import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import Form from "./styles/Form";
-import Error from "./ErrorMessage";
+import PropTypes from "prop-types";
 
-const REQUEST_RESET_MUTATION = gql`
-  mutation REQUEST_RESET_MUTATION($email: String!) {
-    requestReset(email: $email) {
-      message
+const CURRENT_USER_QUERY = gql`
+  query {
+    me {
+      id
+      email
+      name
+      permissions
+
+      cart {
+        id
+        quantity
+        item {
+          id
+          price
+          image
+          title
+          description
+        }
+      }
     }
   }
 `;
-
-class RequestReset extends Component {
-  state = {
-    email: ""
-  };
-  saveToState = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-  render() {
-    return (
-      <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
-        {(reset, { error, loading, called }) => (
-          <Form
-            method="post"
-            data-test="form"
-            onSubmit={async e => {
-              e.preventDefault();
-              await reset();
-              this.setState({ email: "" });
-            }}
-          >
-            <fieldset disabled={loading} aria-busy={loading}>
-              <h2>Request a password reset</h2>
-              <Error error={error} />
-              {!error && !loading && called && (
-                <p>Success! Check your email for a reset link!</p>
-              )}
-              <label htmlFor="email">
-                Email
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  value={this.state.email}
-                  onChange={this.saveToState}
-                />
-              </label>
-
-              <button type="submit">Request Reset!</button>
-            </fieldset>
-          </Form>
-        )}
-      </Mutation>
-    );
-  }
-}
-
-export default RequestReset;
-export { REQUEST_RESET_MUTATION };
+const User = props => (
+  <Query {...props} query={CURRENT_USER_QUERY}>
+    {payload => props.children(payload)}
+  </Query>
+);
+User.propTypes = {
+  children: PropTypes.func.isRequired
+};
+export default User;
+export { CURRENT_USER_QUERY };
